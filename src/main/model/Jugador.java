7b0cbfbfe7bf560243adab0d20c560ab7a1267a5
smosquera1 <smosqueram1@est.ups.edu.ec>
2024-12-02 19:18:47 -0500

    añadir comentario y readme

--- src/main/model/Jugador.java
+++ src/main/model/Jugador.java
@@ -1,12 +1,15 @@
 package main.model;
 
+// Clase que representa a un jugador del juego
 public class Jugador {
-    private String nombre;
+    private String nombre; // Nombre del jugador
 
+    // Constructor: inicializa al jugador con un nombre
     public Jugador(String nombre) {
         this.nombre = nombre;
     }
 
+    // Método para obtener el nombre del jugador
     public String getNombre() {
         return nombre;
     }
