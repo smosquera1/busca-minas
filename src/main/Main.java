2f981bfe55661b732336fba4c7f81b38e6195e38
Jaime Mauricio Carvajal Flores <jcarvajalf2@est.ups.edu.ec>
2024-12-02 22:18:47 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,18 @@
 package main;
 
+import main.controller.JuegoController;
+
 public class Main {
 
+	/**
+	 * Punto de entrada principal del programa.
+	 * Crea una instancia del controlador del juego y lo inicializa.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hola mundo");
-	}
+		// Crear una instancia del controlador del juego
+		JuegoController controller = new JuegoController();
 
+		// Iniciar el juego llamando al método principal del controlador
+		controller.iniciarJuego();
+	}
 }
